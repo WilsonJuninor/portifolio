611c67eb8e65a06ffff09086ab0fc836e4ea2562
Wilson Ferreira <wilsonbareto72@gmail.com>
2025-01-19 19:36:44 -0300

    fist commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 
+/* TUERN THE MENU HAMBURGUER INTO X*/
+const menuHamburguer= document.querySelector (' .menu-hamburguer')
+menuHamburguer.addEventListener('click', () => {
+    toggleMenu();
+});
 
-alert("BUENAS"):
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+
+    if(menuHamburguer.classList.contains('change')){
+        nav.style.display = 'block';
+
+    } else {
+        nav.style.display = 'none';
+    }
+}
